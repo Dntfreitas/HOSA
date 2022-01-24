761d6946b077f2f3f4cd33602951e5ee090b1bc9
Diogo Freitas <dntfreitas123@gmail.com>
2022-01-24 19:30:19 +0000

    Updated files for automated testing.

--- unit_tests.py
+++ unit_tests.py
@@ -276,7 +276,7 @@ def run_hosa_regression():
 class ModelTesting(unittest.TestCase):
 
     def test_cnn_regression(self):
-        self.assertEqual(run_regression_cnn(), False)
+        self.assertEqual(run_regression_cnn(), True)
 
 
 if __name__ == '__main__':
